3744a303534ae868ff4357127094da3a2d3da04f
Brandon <brandcomin@gmail.com>
2025-01-18 17:39:45 -0800

    add initial limelight stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.Vision;
 
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
@@ -34,6 +35,8 @@ public class RobotContainer {
     private final CommandXboxController joystick = new CommandXboxController(0);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
+    public final Vision vision = new Vision();
 
     public RobotContainer() {
         configureBindings();
---
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LimelightHelpers;
+
+public class Vision extends SubsystemBase {
+
+  public enum LedState {
+    ON,
+    OFF,
+    BLINK
+  }
+
+  public Vision() {
+    switchPipeline(0);
+  }
+
+  @Override
+  public void periodic() {
+    // Basic targeting data
+    double tx = LimelightHelpers.getTX(""); // Horizontal offset from crosshair to target in degrees
+    double ty = LimelightHelpers.getTY(""); // Vertical offset from crosshair to target in degrees
+    double ta = LimelightHelpers.getTA(""); // Target area (0% to 100% of image)
+    boolean hasTarget = LimelightHelpers.getTV(""); // Do you have a valid target?
+
+    System.out.println(String.format("tx: %d ty: %d ta: %d hasTarget: %b", tx, ty, ta, hasTarget));
+  }
+
+  /**
+   * Controls the LED on the limelight
+   * 
+   * @param led_state On, Off, BLink
+   */
+  public void controlLED(LedState led_state) {
+    switch (led_state) {
+      case ON:
+        LimelightHelpers.setLEDMode_ForceOn("");
+        break;
+      case OFF:
+        LimelightHelpers.setLEDMode_ForceOff("");
+        break;
+      case BLINK:
+        LimelightHelpers.setLEDMode_ForceBlink("");
+        break;
+      default:
+        LimelightHelpers.setLEDMode_ForceOff("");
+        break;
+    }
+  }
+
+  /**
+   * Switch between pipelines
+   * @param pipeline The pipeline you want to switch to
+   */
+  public void switchPipeline(int pipeline) {
+    LimelightHelpers.setPipelineIndex("", 0);
+  }
+
+}
