4bdbf222d0e385f349f98334a6632d424b69d11a
Jamie Manalang <jamie.manalang@ht-la.org>
2025-02-13 18:56:51 -0800

    Worked on elevator and made LevelSetPoints command to go with

---
+++ src/main/java/frc/robot/Commands/LevelSetPoints.java
@@ -0,0 +1,27 @@
+package frc.robot.Commands;
+import java.lang.Math;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Elevator;
+
+public class LevelSetPoints extends Command {
+  Elevator m_elevator = new Elevator();
+  double m_setpoint = 0;
+
+  public LevelSetPoints(Elevator elevator, double level) {
+    m_elevator = elevator;
+    m_setpoint = level;
+    addRequirements(elevator);
+  }
+
+
+  @Override
+  public void initialize() {
+    m_elevator.goToSetPoint(m_setpoint);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_elevator.setMotors(0);
+  }
+
+}
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,11 +1,64 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ElevatorConstants;
 
 public class Elevator extends SubsystemBase{
-    TalonFX m_rightElevator = new TalonFX(ElevatorConstants.rightElevatorId);
-    TalonFX m_leftElevator = new TalonFX(ElevatorConstants.leftElevatorId);
+    TalonFX m_rightFollower;
+    TalonFX m_leftLeader;
+
+    public MotorOutputConfigs currentConfigs = new MotorOutputConfigs();
+
+    private PositionDutyCycle m_leftDutyCycle;
+
+    double target = 0;
+
+    PositionVoltage m_request;
+
+    public static enum Levels{
+        LEVEL_1,
+        LEVEL_2,
+        LEVEL_3,
+        LEVEL_4,
+        CORAL_STATION
+    }
+
+    public Elevator(){    
+        m_rightFollower = new TalonFX(ElevatorConstants.rightFollowerId);
+        m_leftLeader = new TalonFX(ElevatorConstants.leftLeaderId);
+
+        m_rightFollower.setControl(new Follower(ElevatorConstants.leftLeaderId, true));
+
+        m_request = new PositionVoltage(target).withSlot(0);
+        
+        currentConfigs.Inverted = InvertedValue.Clockwise_Positive;
+        m_leftLeader.getConfigurator().apply(currentConfigs);
+        
+        m_rightFollower.setNeutralMode(NeutralModeValue.Brake);
+        m_leftLeader.setNeutralMode(NeutralModeValue.Brake);
+    }
+
+    public void setMotors(double speed) {
+        m_leftLeader.set(speed);        
+     }
+
+
+    public void goToSetPoint(double setPoint){
+        target = setPoint;
+        m_leftLeader.setControl(m_request.withPosition(setPoint));
+    } 
+
+    public double getCurrentPosition() {
+        return m_leftLeader.getPosition().getValueAsDouble();
+      }
+
 }
