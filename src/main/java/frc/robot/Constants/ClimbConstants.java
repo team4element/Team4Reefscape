b05332eb2c644515fc03a41ef3a957123949e8fa
Railya Farakhova <railya.farakhova@ht-la.org>
2025-03-25 17:43:14 -0700

    made a working climb

--- src/main/java/frc/robot/Constants/ClimbConstants.java
+++ src/main/java/frc/robot/Constants/ClimbConstants.java
@@ -6,5 +6,6 @@ package frc.robot.Constants;
 
 /** Add your docs here. */
 public class ClimbConstants {
-    public static final int climbID = 0;
+    public static final int climbID = 19;
+    public static final int leftID = 20;
 }
--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 /** Add your docs here. */
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -11,6 +12,7 @@ import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -20,57 +22,81 @@ import frc.robot.Constants.ClimbConstants;
 import frc.robot.Constants.ElevatorConstants;
 
 public class Climb extends SubsystemBase{
-    TalonFX m_climb;
+    TalonFX m_right;
+    TalonFX m_left;
     CurrentLimitsConfigs m_limitConfig = new CurrentLimitsConfigs();
     private DutyCycleOut m_dutyCycle;
     private PositionVoltage m_request;
+    private TalonFXConfiguration config;
+    private double m_hold_value;
 
     //2.7 is max rotation or else brake battery
 
     public Climb(){
-        m_climb = new TalonFX(ClimbConstants.climbID);
+        m_right = new TalonFX(ClimbConstants.climbID);
+        m_left = new TalonFX(ClimbConstants.leftID);
         m_dutyCycle = new DutyCycleOut(.5);
         m_request = new PositionVoltage(0);
         TalonFXConfiguration config = new TalonFXConfiguration();
+        TalonFXConfiguration config2 = new TalonFXConfiguration();
+        config2.MotorOutput.withNeutralMode(NeutralModeValue.Brake);
+        config2.MotorOutput.withInverted(InvertedValue.CounterClockwise_Positive);
+        config2.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
         config.MotorOutput.withNeutralMode(NeutralModeValue.Brake);
         config.MotorOutput.withInverted(InvertedValue.Clockwise_Positive);
+        config.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
         config.Slot0.kP = 1;
+        config2.Slot0.kP = 1;
 
-        TalonFXConfigurator configurator = m_climb.getConfigurator();
-        m_climb.getConfigurator().apply(config);
+        TalonFXConfigurator configurator = m_right.getConfigurator();
+        m_right.getConfigurator().apply(config);
+        m_left.getConfigurator().apply(config2);
         m_limitConfig.StatorCurrentLimit = ElevatorConstants.statorLimit;
         m_limitConfig.StatorCurrentLimitEnable = true;
         configurator.apply(m_limitConfig);
     }
     public void runMotorUp(double speed){
-       // if (m_climb.getPosition().getValueAsDouble() <= 2.7){
-          //  m_climb.setControl(m_dutyCycle.withOutput(0));
-      //  } else {
-            m_climb.setControl(m_dutyCycle.withOutput(-speed));
-      //  }
+       if (m_right.getPosition().getValueAsDouble() >= 70){
+           m_right.setControl(m_dutyCycle.withOutput(0));
+           m_left.setControl(m_dutyCycle.withOutput(0));
+           m_right.setNeutralMode(NeutralModeValue.Brake);
+           m_left.setNeutralMode(NeutralModeValue.Brake);
+       } else {
+            m_right.setControl(m_dutyCycle.withOutput(speed));
+            m_left.setControl(m_dutyCycle.withOutput(speed));
+       }
     }
 
     public void runMotorDown(double speed){
-     //  if(m_climb.getPosition().getValueAsDouble() >= 58) {
-       //     m_climb.setControl(m_dutyCycle.withOutput(0));
-     //   } else {
-            m_climb.setControl(m_dutyCycle.withOutput(speed));
-     //   }
+       if(m_right.getPosition().getValueAsDouble() <= -37.2) {
+           m_right.setControl(m_dutyCycle.withOutput(0));
+           m_left.setControl(m_dutyCycle.withOutput(0));
+        } else {
+            m_right.setControl(m_dutyCycle.withOutput(-speed));
+            m_left.setControl(m_dutyCycle.withOutput(-speed));
+        }
     }
+
     public void motorOff(){
-        m_climb.setControl(m_dutyCycle.withOutput(0));
+        m_right.setControl(m_dutyCycle.withOutput(0));
+        m_left.setControl(m_dutyCycle.withOutput(0));
+        m_right.setNeutralMode(NeutralModeValue.Brake);
+           m_left.setNeutralMode(NeutralModeValue.Brake);
     }
     public void manualPivot(double speed){
         final double max_speed = .1;
-        m_climb.setControl(m_dutyCycle.withOutput(speed * max_speed));
+        m_right.setControl(m_dutyCycle.withOutput(speed * max_speed));
+        m_left.setControl(m_dutyCycle.withOutput(speed * max_speed));
     }
 
-    public void goToSetPoint(double setPoint){
-        m_climb.setControl(m_request.withPosition(setPoint));
+    public void goToSetPoint(double setPoint, double goal){
+        m_right.setControl(m_request.withPosition(setPoint));
+        m_left.setControl(m_request.withPosition(goal));
       }
 
     public void reset(){
-        m_climb.setPosition(0);
+        m_right.setPosition(0);
+        m_left.setPosition(0);
     }
 
     @Override
